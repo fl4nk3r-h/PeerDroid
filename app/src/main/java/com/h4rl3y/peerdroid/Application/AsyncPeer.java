b1d53aab0c8579c447d7b5aa7b94813d00db6a10
fl4nk3r-h <fl4nk3r.7h3.b4ckb0n3@gmail.com>
2026-09-06 15:57:31 +0530

    feat: add CryptoUtils for cryptographic operations and implement AES encryption method

--- app/src/main/java/com/h4rl3y/peerdroid/Application/AsyncPeer.java
+++
@@ -1,4 +0,0 @@
-package com.h4rl3y.peerdroid.Application;
-
-public class AsyncPeer {
-}
--- app/src/main/java/com/h4rl3y/peerdroid/crypto/CryptoUtils.java
+++ app/src/main/java/com/h4rl3y/peerdroid/crypto/CryptoUtils.java
@@ -1,14 +1,16 @@
-package com.h4rl3y.peerdroid.Crypto;
+package com.h4rl3y.peerdroid.crypto;
 
 import java.util.Base64;
 
 import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
 
 /**
  * Utility class for cryptographic operations.
  * Status: In Development
  * Location: com.h4rl3y.peerdroid.Crypto
- * Description: This class provides utility methods for performing cryptographic operations.
+ * Description: This class provides utility methods for performing cryptographic
+ * operations.
  * 
  * @author @fl4nk3r-h
  * @version 1.0
@@ -16,16 +18,16 @@ import javax.crypto.Cipher;
 public class CryptoUtils {
     /**
      * Encrypts the given data using AES encryption with the provided secret key.
-     *
+     * 
      * @param data The data to encrypt.
      * @param key  The secret key to use for encryption.
      * @return The encrypted data as a Base64-encoded string.
      * @throws Exception If an error occurs during encryption.
      */
-    public static String encrypt(String data, SecretKey key)throws Exception {
+    public static String encrypt(String data, SecretKey key) throws Exception {
         Cipher cipher = Cipher.getInstance("AES");
         cipher.init(Cipher.ENCRYPT_MODE, key);
         byte[] encryptedData = cipher.doFinal(data.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedData); // Placeholder return statement
+        return Base64.getEncoder().encodeToString(encryptedData); 
     }
 }
