4d415fbb9e89377d43063bdb0671ce424c0e21ff
h4rl3y-q <bhattacharyapriyanka2005@gmail.com>
2026-09-08 15:31:46 +0530

    feat: add Message class for chat message representation with timestamp

--- app/src/main/java/com/h4rl3y/peerdroid/model/Message.java
+++ app/src/main/java/com/h4rl3y/peerdroid/model/Message.java
@@ -1,4 +1,23 @@
 package com.h4rl3y.peerdroid.model;
 
+/**
+ * Immutable chat message used by the RecyclerView adapter to display messages in the chat.
+ *
+ * @author @h4rl3y-q
+ * @version 1.0
+ * @see com.h4rl3y.peerdroid.ui.MessageAdapter
+ */
 public class Message {
+    public final String text;
+    public final boolean isSelf;
+    public final long timestampMs;
+
+    public Message(String text, boolean isSelf) {
+        this(text, isSelf, System.currentTimeMillis());
+    }
+    public Message(String text, boolean isSelf, long timestampMs) {
+        this.text = text;
+        this.isSelf = isSelf;
+        this.timestampMs = timestampMs;
+    }
 }
